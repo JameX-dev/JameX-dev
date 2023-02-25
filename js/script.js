6d4565a9bd3da88f0a7ab204546d24659b4c3665
Jaime Navarro <jaime_navarro2208@hotmail.com>
2023-02-24 20:42:38 -0600

    traduccion de la pagina

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ menu.onclick = () =>{
     menu.classList.toggle('bx-x')
     navbar.classList.toggle('open')
 }
+
+const flagElement = document.getElementById("flags")
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (languague)=>{
+   
+    const requestJson = await fetch(`./ languages/${languague}.json`);
+    const texts = await requestJson.json();
+
+    
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+   
+}
+flagElement.addEventListener("click", (e) =>{
+    changeLanguage(e.target.parentElement.dataset.languague)
+});
